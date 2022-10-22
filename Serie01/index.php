ef4a284dcbd47de0cd518e498a82db9bc57409e1
VitalRenock <renaud.brigode@gmail.com>
2022-10-22 18:48:41 +0200

    Serie01_Création

---
+++ Serie01/index.php
@@ -0,0 +1,18 @@
+<?php
+    $maVariable = 'Renaud'
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <title>Ma première page</title>
+    </head>
+
+    <body>
+        <h1>Ma première page Web</h1>
+        <p>Bienvenue sur mon site <?php echo '<b style="color: darkgreen">'.$maVariable.'</b>' ?></p>
+    </body>
+
+</html>
+
